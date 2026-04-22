258d818517ea468f3204c98c96229914ea35f4c1
AeroX <aeroxdevs@gmail.com>
2026-04-22 17:46:54 +0530

    Initial commit: add src/events/discord/guild/messageCounter.js

---
+++ src/events/discord/guild/messageCounter.js
@@ -0,0 +1,35 @@
+// Falcron | AeroX Development
+// Author: itsfizys
+import { db } from '#dbManager';
+
+export default {
+        name: 'messageCreate',
+        async execute({ eventArgs }) {
+                if (!eventArgs?.[0]) return;
+
+                const [message] = eventArgs;
+
+                if (message.author?.bot || !message.guild) return;
+
+                try {
+                        const isBlacklisted = await db.guild?.isMessageChannelBlacklisted(
+                                message.guild.id,
+                                message.channel.id,
+                        );
+                        if (isBlacklisted) return;
+
+                        await db.userMessageCounter?.increment(message.guild.id, message.author.id);
+                } catch {
+                }
+        },
+};
+
+/**
+ * Project: Falcron
+ * Author: itsfizys (Aegis)
+ * Organization: AeroX Development
+ * GitHub: https://github.com/AeroXDevs
+ * License: Custom
+ *
+ * © 2026 AeroX Development. All rights reserved.
+ */
