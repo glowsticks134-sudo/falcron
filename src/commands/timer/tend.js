ef70863ffec665dfce6958427b37761a0970fa3a
AeroX <aeroxdevs@gmail.com>
2026-05-04 21:19:35 +0530

    fix: add permission checks to commands

--- src/commands/timer/tend.js
+++ src/commands/timer/tend.js
@@ -1,7 +1,7 @@
 // Falcron | AeroX Development
 // Author: itsfizys
 import { Command } from '#command';
-import { MessageFlags } from 'discord.js';
+import { MessageFlags, PermissionFlagsBits } from 'discord.js';
 import { emoji } from '#emoji';
 import { timerStore, buildEndComponents, formatTime } from '#timerUtils';
 
@@ -14,10 +14,13 @@ class TEndCommand extends Command {
                         cooldown: 5,
                         minArgs: 1,
                         examples: ['tend 1150000000000000000'],
+                        userPermissions: [PermissionFlagsBits.ManageGuild],
+                        permissions: [PermissionFlagsBits.ManageGuild],
                         enabledSlash: true,
                         slashData: {
                                 name: 'tend',
                                 description: 'Ends an active timer',
+                                defaultMemberPermissions: PermissionFlagsBits.ManageGuild,
                                 options: [
                                         {
                                                 type: 3,
